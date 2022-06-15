edf0f908d3b1b4886ebab9d8bf7fd49d155f2391
Andrew Pobrica <pirit66@gmail.com>
2022-06-15 18:22:14 -0400

    Added conditional formatting to initialize and a verifyValid function

--- RUBIKS.c
+++ RUBIKS.c
@@ -7,19 +7,21 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include <time.h>
 #include "mpi.h"
 #include "RUBIKS.h"
 #include "HELPERS.h"
 
 void initialize(cube_t* cube);
+void verifyValid(cube_t* cube);
 
 int main(int argc, char * argv[]) {
     cube_t *my_cube = malloc(N * N * N * 6 * sizeof(int));
     initialize(my_cube);
 
 
-
+    verifyValid(my_cube);
 
 
     free(my_cube);
@@ -29,7 +31,6 @@ void initialize(cube_t* cube){
     for (int i = 0; i < N; i++){
         for (int j = 0; j < N; j++) {
             for (int k = 0; k < N; k ++){
-                for (int l = 0; l < N; l++){
                     if (i == 0)
                         (*cube)[i][j][k][TOP] = WHITE;
                     else
@@ -38,23 +39,60 @@ void initialize(cube_t* cube){
                         (*cube)[i][j][k][LEFT] = GREEN;
                     else
                         (*cube)[i][j][k][LEFT] = NONE;
-                    if ( l == 0)
-                        (*cube)[i][j][l][FRONT] = RED;
+                    if ( k == 0)
+                        (*cube)[i][j][k][FRONT] = RED;
                     else
                         (*cube)[i][j][k][FRONT] = NONE;
                     if ( j == N-1)
-                        (*cube)[i][j][l][RIGHT] = BLUE;
+                        (*cube)[i][j][k][RIGHT] = BLUE;
                     else
-                        (*cube)[i][j][l][RIGHT] = NONE;
-                    if ( l == N-1)
-                        (*cube)[i][j][l][BACK] = ORANGE;
+                        (*cube)[i][j][k][RIGHT] = NONE;
+                    if ( k == N-1)
+                        (*cube)[i][j][k][BACK] = ORANGE;
                     else
-                        (*cube)[i][j][l][BACK] = NONE;
+                        (*cube)[i][j][k][BACK] = NONE;
                     if ( i == N-1)
-                        (*cube)[i][j][l][BOTTOM] = YELLOW;
+                        (*cube)[i][j][k][BOTTOM] = YELLOW;
                     else
-                        (*cube)[i][j][l][BOTTOM] = NONE;
-                }
+                        (*cube)[i][j][k][BOTTOM] = NONE;
+            }
+        }
+    }
+}
+
+void scramble(cube_t* cube){
+
+}
+
+void verifyValid(cube_t* cube) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            for (int k = 0; k < N; k++) {
+                if (i == 0)
+                    assert((*cube)[i][j][k][TOP] != NONE);
+                else
+                    assert((*cube)[i][j][k][TOP] == NONE);
+                if (j == 0)
+                    assert((*cube)[i][j][k][LEFT] != NONE);
+                else
+                    assert((*cube)[i][j][k][LEFT] == NONE);
+                if (k == 0)
+                    assert((*cube)[i][j][k][FRONT] != NONE);
+                else
+                    assert((*cube)[i][j][k][FRONT] == NONE);
+                if (j == N - 1)
+                    assert((*cube)[i][j][k][RIGHT] != NONE);
+                else
+                    assert((*cube)[i][j][k][RIGHT] == NONE);
+                if (k == N - 1)
+                    assert((*cube)[i][j][k][BACK] != NONE);
+                else
+                    assert((*cube)[i][j][k][BACK] == NONE);
+                if (i == N - 1)
+                    assert((*cube)[i][j][k][BOTTOM] != NONE);
+                else
+                    assert((*cube)[i][j][k][BOTTOM] == NONE);
+
             }
         }
     }
