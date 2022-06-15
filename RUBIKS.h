943821e8adb4f7dd24f2d5817309a0e5c6d4d795
clarkeap <clarkeap@appstate.edu>
2022-06-15 14:29:51 -0400

    working on headers

--- RUBIKS.h
+++ RUBIKS.h
@@ -5,4 +5,14 @@
 #ifndef RUBIKS_BUSTER_RUBIKS_H
 #define RUBIKS_BUSTER_RUBIKS_H
 
+  enum Color {WHITE, GREEN, RED, BLUE, ORANGE, YELLOW};
+  typedef Color color_t;
+
+  enum Side {TOP, LEFT, FRONT, RIGHT, BACK, BOTTOM};
+  typedef Side side_t;
+
+  color_t Cube[48];
+
+  // Prototypes
+
 #endif //RUBIKS_BUSTER_RUBIKS_H
