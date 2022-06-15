ba806b278da383f7a19a134737fe933aba177f32
Andrew Pobrica <pirit66@gmail.com>
2022-06-15 18:04:26 -0400

    Added conditional formatting to initialize

--- RUBIKS.c
+++ RUBIKS.c
@@ -12,8 +12,17 @@
 #include "RUBIKS.h"
 #include "HELPERS.h"
 
+void initialize(cube_t* cube);
+
 int main(int argc, char * argv[]) {
     cube_t *my_cube = malloc(N * N * N * 6 * sizeof(int));
+    initialize(my_cube);
+
+
+
+
+
+    free(my_cube);
 
 }
 void initialize(cube_t* cube){
@@ -21,12 +30,30 @@ void initialize(cube_t* cube){
         for (int j = 0; j < N; j++) {
             for (int k = 0; k < N; k ++){
                 for (int l = 0; l < N; l++){
-                    (*cube)[i][j][k][TOP] = WHITE;
-                    (*cube)[i][j][k][LEFT] = GREEN;
-                    (*cube)[i][j][l][FRONT] = RED;
-                    (*cube)[i][j][l][RIGHT] = BLUE;
-                    (*cube)[i][j][l][BACK] = ORANGE;
-                    (*cube)[i][j][l][BOTTOM] = YELLOW;
+                    if (i == 0)
+                        (*cube)[i][j][k][TOP] = WHITE;
+                    else
+                        (*cube)[i][j][k][TOP] = NONE;
+                    if (j == 0)
+                        (*cube)[i][j][k][LEFT] = GREEN;
+                    else
+                        (*cube)[i][j][k][LEFT] = NONE;
+                    if ( l == 0)
+                        (*cube)[i][j][l][FRONT] = RED;
+                    else
+                        (*cube)[i][j][k][FRONT] = NONE;
+                    if ( j == N-1)
+                        (*cube)[i][j][l][RIGHT] = BLUE;
+                    else
+                        (*cube)[i][j][l][RIGHT] = NONE;
+                    if ( l == N-1)
+                        (*cube)[i][j][l][BACK] = ORANGE;
+                    else
+                        (*cube)[i][j][l][BACK] = NONE;
+                    if ( i == N-1)
+                        (*cube)[i][j][l][BOTTOM] = YELLOW;
+                    else
+                        (*cube)[i][j][l][BOTTOM] = NONE;
                 }
             }
         }
