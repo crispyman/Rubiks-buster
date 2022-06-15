3386236e16c88ed1604016092c04283169fc9fdf
Andrew Pobrica <pirit66@gmail.com>
2022-06-15 14:32:11 -0400

    Started RUBIKS.c added makefile

--- RUBIKS.c
+++ RUBIKS.c
@@ -2,4 +2,16 @@
 // Created by andrewiii on 6/15/22.
 //
 
+#include <stdio.h>
+#include <jerror.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mpi.h"
 #include "RUBIKS.h"
+
+int main(int argc, char * argv[]){
+
+
+}
+
