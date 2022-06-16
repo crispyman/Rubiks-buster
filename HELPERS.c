0d5f50d528defdc0b6d26d22cb05d3d934cd1d03
Alex Clarke <clarkeap@appstate.edu>
2022-06-16 13:00:31 -0400

    got rotate mostly implemented

--- HELPERS.c
+++ HELPERS.c
@@ -1,10 +1,50 @@
 #include "HELPERS.h"
 
-//  cube    - a pointer to the cube.
-//  side    - which side we want to rotate.
-//  cc      - whether the rotation is counter clockwise or not.
-void rotate(cube_t* cube, side_t side, int cc) {
-  
+void rotate(cube_t* cube, coord_t coord, axes_t a, int cc) {
+  sub_cube_t* plane[N][N];
+
+  // Load the requested plane.
+  load_plane(cube, coord.x, coord.y, coord.z, a, plane);
+
+  // Rotate position of the subcubes within the plane.
+  rotate_plane(plane, cc);
+
+  // Determine which direction the sub cubes need to rotate.
+  rotate_t r;
+  if (a == XY) r = cc ? R_LEFT : R_RIGHT;
+  if (a == YZ) r = cc ? R_RIGHT : R_LEFT;
+  if (a == YZ) r = cc ? R_DOWN : R_UP;
+
+  // Rotate the faces of each affected sub cube.
+  for (int i = 0; i < N; i++)
+    for (int j = 0; j < N; j++)
+      rotate_sub_cube(plane[i][j], r);
+}
+
+// Given a single subcube's coordinates and a set of two axes, load the
+// addresses of all the coplanar subcubes into the array plane.
+void load_plane(cube_t* cube, int x, int y, int z, axes_t a, sub_cube_t* plane[N][N]) {
+  for (int y_i = 0; y_i < N; y_i++) {
+    for (int x_i = 0; x_i < N; x_i++) {
+      for (int z_i = 0; z_i < N; z_i++) {
+        int y_val = a == XZ ? y : y_i;
+        int x_val = a == YZ ? x : x_i;
+        int z_val = a == XY ? z : z_i;
+        plane[y][x] = &cube[y_val][x_val][z_val];
+      }
+    }
+  }
+}
+
+// Given a populated plane, rotate it 90% either clockwise or counter clockwise.
+void rotate_plane(sub_cube_t* plane[N][N], int cc) {
+  sub_cube_t* new_plane[N][N];
+
+  for (int y_i; y_i < N; y_i++)
+    for (int x_i; x_i < N; x_i++)
+      new_plane[y_i][x_i] = cc ? plane[N - x][y] : plane[x][N - y];
+
+  memcpy(plane, new_plane, sizeof(sub_cube_t) * N * N);
 }
 
 // Rotate a sub cube.
--- HELPERS.h
+++ HELPERS.h
@@ -5,11 +5,19 @@
   typedef enum {NONE, WHITE, GREEN, RED, BLUE, ORANGE, YELLOW} color_t;
   typedef enum {TOP, LEFT, FRONT, RIGHT, BACK, BOTTOM} side_t;
   typedef enum {R_UP, R_DOWN, R_LEFT, R_RIGHT} rotate_t;
+  typedef enum {XY, XZ, YZ} axes_t;
   typedef color_t sub_cube_t[6];
   typedef sub_cube_t cube_t[N][N][N];
+
+  typedef struct {
+    uint x;
+    uint y;
+    uint z;
+  } coord_t;
 
   // Prototypes
   void rotate(cube_t* cube, side_t side, int cc);
   void rotate_sub_cube(color_t* sub_cube, rotate_t* rotation);
+  void load_plane(cube_t* cube, int x, int y, int z, axis_t axis, sub_cube_t* plane[N][N]);
 
 #endif //RUBIKS_BUSTER_HELPERS_H
