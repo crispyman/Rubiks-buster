f4a89d4cc99b8179cda0775b8ef85fd8c6e0c28f
Alex Clarke <clarkeap@appstate.edu>
2022-06-22 18:12:43 -0400

    refactor sequential

--- RUBIKS_SEQENTIAL.h
+++ RUBIKS_SEQENTIAL.h
@@ -5,9 +5,7 @@
 #ifndef RUBIKS_BUSTER_RUBIKS_SEQENTIAL_H
 #define RUBIKS_BUSTER_RUBIKS_SEQENTIAL_H
 
-solution_t* seqentialLauncher(cube_t* cube);
-void seqentialSolver(cube_t *cube, rotate_action_t action, int step);
-
-
+  solution_t* seqentialLauncher(cube_t* cube);
+  void seqentialSolver(cube_t* cube, rotate_action_t action, int step);
 
 #endif //RUBIKS_BUSTER_RUBIKS_SEQENTIAL_H
