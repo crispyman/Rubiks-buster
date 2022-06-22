89f6f4c7513e371a65441ce36bdbb8defc03fb74
Alex Clarke <clarkeap@appstate.edu>
2022-06-22 16:28:38 -0400

    added timer and speedup calculation

--- RUBIKS.c
+++ RUBIKS.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <time.h>
 #include "mpi.h"
 #include "RUBIKS.h"
 #include "HELPERS.h"
@@ -19,8 +20,13 @@ void scramble(cube_t* cube);
 
 
 int main(int argc, char * argv[]) {
+    double seq_time, para_time;
+
     // Set the random seed.
     srand(time(0));
+
+    // Define the timer.
+    clock_t t;
 
     // Initialize the cube.
     cube_t *my_cube = malloc(sizeof(cube_t));
@@ -37,18 +43,49 @@ int main(int argc, char * argv[]) {
     print_cube(my_cube);
     printf("\n");
 
-    // Call the sequential solver on the scrambled cube.
-    cube_t * seq_cube = malloc(sizeof(cube_t));
+    // Make a copy of the scrambled cube for the sequential version to work on.
+    cube_t* seq_cube = malloc(sizeof(cube_t));
     memcpy(seq_cube, my_cube, sizeof(cube_t));
-    solution_t * seq_solution = seqentialLauncher(seq_cube);
-    if (seq_solution->steps){
+
+    // Run the sequential solver and time its execution.
+    t = clock();
+    solution_t* seq_solution = seqentialLauncher(seq_cube);
+    t = clock() - t;
+
+    // Free the sequential copy of the cube.
+    free(seq_cube);
+
+    // Convert the time to seconds.
+    seq_time = ((double)t) / CLOCKS_PER_SEC;
+
+    // Call the sequential solver on the scrambled cube.
+    if (seq_solution->steps) {
         printf("Solved in: %d steps\n", seq_solution->length);
     } else {
         printf("No Solution in %d steps\n", MAX_SOLUTION_LENGTH);
     }
-    
-    // Call the sequential solver on the scrambled cube.
 
+    // Make a copy of the scrambled cube for the parallel version to work on.
+    cube_t* para_cube = malloc(sizeof(cube_t));
+    memcpy(para_cube, my_cube, sizeof(cube_t));
+
+    // Run the parallel solver and time its execution.
+    t = clock();
+    // PLACE CALL TO PARALLEL HERE
+    t = clock() - t;
+
+    // Free the sequential copy of the cube.
+    free(para_cube);
+
+    // Convert the time to seconds.
+    para_time = ((double)t) / CLOCKS_PER_SEC;
+
+    double speedup = seq_time / para_time;
+    printf("Sequential speed:\t%fs\n", seq_time);
+    printf("Parallel speed:\t%fs\n", para_time);
+    printf("Speedup:\t%fs\n", seq_time / para_time);
+
+    // Free the original cube.
     free(my_cube);
 }
 
