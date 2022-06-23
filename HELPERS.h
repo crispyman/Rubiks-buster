97d599742a4c0753f54b6f30d98965a2e1b53f9e
Andrew Pobrica <pirit66@gmail.com>
2022-06-22 22:48:43 -0400

    Finalization stuff

--- HELPERS.h
+++ HELPERS.h
@@ -10,8 +10,8 @@
     #define SIDES 6
     #define N 3
     #define DEBUG 0
-    #define N_SCRAMBLES 6
-    #define MAX_SOLUTION_LENGTH 6
+    #define N_SCRAMBLES 200
+    #define MAX_SOLUTION_LENGTH 5
     #define MAX_STACK_SIZE 20
 
     /* ----- Type Definitions ----- */
